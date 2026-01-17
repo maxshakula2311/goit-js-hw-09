734249c3a1749ee86e992e7dd08256cc82832a12
max-shakula <maxshakulakr@gmail.com>
2026-01-17 15:22:02 +0200

    done (maybe)

---
+++ src/js/2-form.js
@@ -0,0 +1,41 @@
+const formData = {
+    email: '',
+    message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+    Object.assign(formData, JSON.parse(savedData));
+    form.email.value = formData.email;
+    form.message.value = formData.message;
+}
+
+form.addEventListener('input', (event) => {
+    const { name, value } = event.target;
+    
+    if (name === 'email' || name === 'message') {
+        formData[name] = value;
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    }
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    if (!formData.email || !formData.message) {
+        alert('Fill all fields please!');
+        return;
+    }
+    
+    console.log(formData);
+    
+    localStorage.removeItem(STORAGE_KEY);
+    
+    formData.email = '';
+    formData.message = '';
+    
+    form.reset();
+});
